24cf7ffab4982e6c6b526746acaffe7b28ffca88
Riderman de Sousa Barbosa <725337+Ridermansb@users.noreply.github.com>
2024-08-26 14:34:03 -0300

    add @vercel/speed-insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "@mantine/dates/styles.css";
 import { ColorSchemeScript } from "@mantine/core";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Providers from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -27,6 +28,7 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
+        <SpeedInsights />
       </body>
     </html>
   );
