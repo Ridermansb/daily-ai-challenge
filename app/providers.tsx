50bf399ab620da6de5072a0c5575a096a742681e
Riderman de Sousa Barbosa <725337+Ridermansb@users.noreply.github.com>
2024-08-25 22:44:50 -0300

    use same queryClient across page and providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,34 +1,12 @@
 "use client";
 
 import { ReactNode } from "react";
-import { isServer, QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createTheme, MantineProvider } from "@mantine/core";
+import { getQueryClient } from "./utils";
 
 const theme = createTheme({});
-
-function makeQueryClient() {
-  return new QueryClient({
-    defaultOptions: {
-      queries: {
-        // With SSR, we usually want to set some default staleTime
-        // above 0 to avoid refetching immediately on the client
-        staleTime: 60 * 1000,
-      },
-    },
-  });
-}
-
-let browserQueryClient: QueryClient | undefined = undefined;
-
-function getQueryClient() {
-  if (isServer) {
-    return makeQueryClient();
-  } else {
-    if (!browserQueryClient) browserQueryClient = makeQueryClient();
-    return browserQueryClient;
-  }
-}
 
 const Providers = ({ children }: { children: ReactNode }) => {
   const queryClient = getQueryClient();
